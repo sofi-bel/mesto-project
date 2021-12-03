779924052085672f65c1305a5e00e8f89c2f5fd8
Anastasiia Metikova <a.meti2301@gmail.com>
2021-12-03 14:37:35 +0700

    Added function to delete a location

--- scripts/index.js
+++ scripts/index.js
@@ -19,26 +19,50 @@ function openModalWindow(evt) {
 }
 
 function closeModalWindow(evt) {
-  if(evt.target.classList.contains('popup__close-button_type_edit-profile')) {
+  const eventClassList = evt.target.classList;
+
+  if (eventClassList.contains('form_type_edit-profile') ||
+    eventClassList.contains('popup__close-button_type_edit-profile')) {
     document.querySelector('.popup_type_edit-profile').classList.remove('popup_opened');
-  } else if(evt.target.classList.contains('popup__close-button_type_add-place')) {
+  } else if (eventClassList.contains('form_type_add-place') ||
+    eventClassList.contains('popup__close-button_type_add-place')) {
     document.querySelector('.popup_type_add-place').classList.remove('popup_opened');
   }
 }
 
-const formElement = document.querySelector('.form');
-formElement.addEventListener('submit', formSubmitHandler);
+function initForm() {
+  document.querySelector('.form_type_edit-profile').addEventListener('submit', UpdateProfile);
+  document.querySelector('.form_type_add-place').addEventListener('submit', addPlace);
+}
 
-function formSubmitHandler(evt) {
+initForm();
+
+function UpdateProfile(evt) {
   evt.preventDefault();
   const profileInfo = document.querySelector('.profile__info');
-  const nameInput = formElement.querySelector('.form__input_type_name');
-  const jobInput = formElement.querySelector('.form__input_type_job');
+  const nameInput = document.querySelector('.form__input_type_name');
+  const jobInput = document.querySelector('.form__input_type_job');
 
   profileInfo.querySelector('.profile__title').textContent = nameInput.value;
   profileInfo.querySelector('.profile__subtitle').textContent = jobInput.value;
 
-  closeModalWindow();
+  closeModalWindow(evt);
+}
+
+function addPlace(evt) {
+  evt.preventDefault();
+  const placeInput = document.querySelector('.form__input_type_place');
+  const linkInput = document.querySelector('.form__input_type_url');
+  const item = [];
+
+  item.name = placeInput.value;
+  item.link = linkInput.value;
+
+  addCard(item);
+  closeModalWindow(evt);
+
+  placeInput.value = '';
+  linkInput.value = '';
 }
 
 function initCards() {
@@ -70,22 +94,33 @@ function initCards() {
   ];
 
   initialCards.forEach(function(item) {
-    const cardTemplate = document.querySelector('#element-template').content;
-    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
-
-    cardElement.querySelector('.element__image').src = item.link;
-    cardElement.querySelector('.element__image').alt = item.name;
-    cardElement.querySelector('.element__title').textContent = item.name;
-
-    const cardLikeButton = cardElement.querySelector('.element__like-button');
-
-    cardLikeButton.addEventListener('click', function (evt) {
-      evt.target.classList.toggle('element__like-button_active');
-    });
-
-    const cardList = document.querySelector('.elements__list');
-    cardList.append(cardElement);
+    addCard(item)
   });
 }
 
 initCards();
+
+function addCard(item) {
+  const cardTemplate = document.querySelector('#element-template').content;
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+  cardElement.querySelector('.element__image').src = item.link;
+  cardElement.querySelector('.element__image').alt = item.name;
+  cardElement.querySelector('.element__title').textContent = item.name;
+
+  const cardLikeButton = cardElement.querySelector('.element__like-button');
+  const cardDeleteButton = cardElement.querySelector('.element__delete-button');
+
+  cardLikeButton.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like-button_active');
+  });
+
+  cardDeleteButton.addEventListener('click', deleteCard);
+
+  const cardList = document.querySelector('.elements__list');
+  cardList.prepend(cardElement);
+}
+
+function deleteCard(evt) {
+  evt.target.closest('.element').remove();
+}
