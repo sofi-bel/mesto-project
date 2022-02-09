f9f8ca1b6343d3ee033ac5457560f29ac47fe71b
Anastasiia Metikova <a.meti2301@gmail.com>
2022-02-09 11:59:40 +0700

    Add webpack, stylelint, and eslint configs

--- README.md
+++ README.md
@@ -1,30 +1,58 @@
-# Проект: Место
+<!--suppress HtmlDeprecatedAttribute, HtmlRequiredAltAttribute -->
+<div align="center">
+  <h1>Mesto</h1>
+  <p>A landing page about Traveling.</p>
+  <a href="https://wakatime.com/badge/user/29a8352f-15fa-421a-b8ff-a7adff87a0dc/project/5c34f503-85a4-4d37-8538-6ddee0351502"><img src="https://wakatime.com/badge/user/29a8352f-15fa-421a-b8ff-a7adff87a0dc/project/5c34f503-85a4-4d37-8538-6ddee0351502.svg?style=for-the-badge&logo=appveyor" alt="wakatime"></a>
+  <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/a-meti/mesto-project?style=for-the-badge">
+  <a href="https://github.com/a-meti/mesto-project/blob/main/LICENSE"><img alt="GitHub license" src="https://img.shields.io/github/license/a-meti/mesto-project?style=for-the-badge"></a>
+  <p>
+      <a href="https://jigsaw.w3.org/css-validator/check/referer">
+          <img style="border:0;width:88px;height:31px"
+              src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
+              alt="Правильный CSS!" />
+      </a>
+  </p>
+</div>
 
-[![wakatime](https://wakatime.com/badge/user/29a8352f-15fa-421a-b8ff-a7adff87a0dc/project/5c34f503-85a4-4d37-8538-6ddee0351502.svg?style=for-the-badge&logo=appveyor)](https://wakatime.com/badge/user/29a8352f-15fa-421a-b8ff-a7adff87a0dc/project/5c34f503-85a4-4d37-8538-6ddee0351502)
-![GitHub last commit](https://img.shields.io/github/last-commit/a-meti/mesto-project?style=for-the-badge&logo=appveyor)
-[![GitHub license](https://img.shields.io/github/license/a-meti/mesto-project?style=for-the-badge&logo=appveyor)](https://github.com/a-meti/mesto-project/blob/main/LICENSE)
-
-Учебный проект для отработки адаптивной верстки в рамках обучения в [Яндекс.Практикуме Веб-разработчик плюс.](https://practicum.yandex.ru/web-plus/)
-Одностраничный сайт о Путешествии по России. Адаптированный под разные расширения экранов от 320px.
-
-## Figma
-
-[Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
-
-## Используемые технологии
-
+## Template for Stack
 * HTML5 & CSS3
+* Pug
+* SCSS
 * JavaScript
 * Flexbox
 * CSS grid
 * BEM
-* BEM Nested
 * Adaptive web design
 * Responsive web design
 * Mobile first
 * Media queries
-* Font smoothing
+* Web accessibility
+* Relative units
+* Semantic layout
+* Webpack
 
+## Build Setup
+Clone the repo
+
+```
+git clone https://github.com/a-meti/mesto-project.git
+```
+Install dependencies
+```
+npm install
+```
+Server with hot reload at http://localhost:9000/
+```
+npm run start
+```
+Production Build
+```
+npm run build
+```
+Development mode
+```
+npm run dev
+```
 ## Demo
 
-[Посмотреть](https://a-meti.github.io/mesto-project/)
+[Look at the result.](https://a-meti.github.io/mesto-project/)
---
+++ src/scripts/index.js
@@ -0,0 +1,167 @@
+import "../scss/index.scss";
+
+const profileEditButton = document.querySelector(".profile__button_type_edit");
+const addNewCardButton = document.querySelector(".profile__button_type_add");
+const closeEditProfilePopupButton = document.querySelector(
+  ".popup__close-button_type_edit-profile",
+);
+const closeAddPlacePopupButton = document.querySelector(
+  ".popup__close-button_type_add-place",
+);
+const closeOpenImagePopupButton = document.querySelector(
+  ".popup__close-button_type_image-place",
+);
+const popupEditProfile = document.querySelector(".popup_type_edit-profile");
+const popupAddPlace = document.querySelector(".popup_type_add-place");
+const popupImagePlace = document.querySelector(".popup_type_image-place");
+const editProfileForm = document.querySelector(".form_type_edit-profile");
+const addPlaceForm = document.querySelector(".form_type_add-place");
+const profileInfo = document.querySelector(".profile__info");
+const nameInput = document.querySelector(".form__input_type_name");
+const jobInput = document.querySelector(".form__input_type_job");
+const profileTitle = profileInfo.querySelector(".profile__title");
+const profileSubtitle = profileInfo.querySelector(".profile__subtitle");
+const placeInput = document.querySelector(".form__input_type_place");
+const linkInput = document.querySelector(".form__input_type_url");
+const imagePopup = document.querySelector(".image-place__image");
+const imageCaption = document.querySelector(".image-place__caption");
+
+profileEditButton.addEventListener("click", handleOpenEditProfile);
+addNewCardButton.addEventListener("click", handleOpenAddPlace);
+closeEditProfilePopupButton.addEventListener("click", handleCloseEditProfile);
+closeAddPlacePopupButton.addEventListener("click", handleCloseAddPlace);
+closeOpenImagePopupButton.addEventListener("click", handleCloseImage);
+
+function handleOpenEditProfile() {
+  nameInput.value = profileTitle.textContent.trim();
+  jobInput.value = profileSubtitle.textContent.trim();
+  openPopup(popupEditProfile);
+}
+
+function handleOpenAddPlace() {
+  openPopup(popupAddPlace);
+}
+
+function handleOpenImage(evt) {
+  imagePopup.src = evt.target.src;
+  imagePopup.alt = evt.target.alt;
+  imageCaption.textContent = evt.target.alt;
+
+  openPopup(popupImagePlace);
+}
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
+function handleCloseEditProfile() {
+  closePopup(popupEditProfile);
+}
+
+function handleCloseAddPlace() {
+  closePopup(popupAddPlace);
+}
+
+function handleCloseImage() {
+  closePopup(popupImagePlace);
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
+
+function initForm() {
+  editProfileForm.addEventListener("submit", updateProfile);
+  addPlaceForm.addEventListener("submit", addPlace);
+}
+
+initForm();
+
+function updateProfile(evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+
+  closePopup(popupEditProfile);
+}
+
+function addPlace(evt) {
+  evt.preventDefault();
+  const item = [];
+
+  item.name = placeInput.value;
+  item.link = linkInput.value;
+
+  addCard(createCard(item));
+  closePopup(popupAddPlace);
+
+  placeInput.value = "";
+  linkInput.value = "";
+}
+
+function initCards() {
+  const initialCards = [
+    {
+      name: "Arkhyz",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+      name: "Chelyabinsk region",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+      name: "Ivanovo",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+      name: "Kamchatka",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+      name: "Kholmogorsky district",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Baikal",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    },
+  ];
+
+  initialCards.forEach(function (item) {
+    addCard(createCard(item));
+  });
+}
+
+initCards();
+
+function addCard(card) {
+  const cardList = document.querySelector(".elements__list");
+
+  cardList.prepend(card);
+}
+
+function createCard(item) {
+  const cardTemplate = document.querySelector("#element-template").content;
+  const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
+  const cardImage = cardElement.querySelector(".element__image");
+  const cardTitle = cardElement.querySelector(".element__title");
+  const cardLikeButton = cardElement.querySelector(".element__like-button");
+  const cardDeleteButton = cardElement.querySelector(".element__delete-button");
+
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  cardTitle.textContent = item.name;
+
+  cardLikeButton.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("element__like-button_active");
+  });
+  cardDeleteButton.addEventListener("click", deleteCard);
+  cardImage.addEventListener("click", handleOpenImage);
+
+  return cardElement;
+}
+
+function deleteCard(evt) {
+  evt.target.closest(".element").remove();
+}
